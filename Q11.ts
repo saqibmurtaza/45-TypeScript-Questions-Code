c1b8cc097fc84a9ddf3780c8e71d15f5de900c2a
Saqib <saqibmurtazakhan@gmail.com>
2023-08-08 15:28:35 +0500

    Q12
    Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
    
    Commit saved

--- Q11.ts
+++ Q11.ts
@@ -1,9 +1,22 @@
+<<<<<<< HEAD
 // HEAD
 
 //
+=======
+import { log } from "console";
+
+>>>>>>> ae9eab1 (Q12)
 //Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
 let names = ['Jawad', 'Mubashir', 'Shahjee'];
 for(let name of names){
     console.log(name);
 }
+<<<<<<< HEAD
 // 8848dc6 (Q-11)
+=======
+// Q12:Greeting 
+for(let name of names){
+    console.log(`Hello ${name} how are you?`);
+}
+//
+>>>>>>> ae9eab1 (Q12)
