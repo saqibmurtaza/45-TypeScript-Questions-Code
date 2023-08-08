58dc48b29253af6a6c10e6c5a6ace63fc46610ba
Saqib <saqibmurtazakhan@gmail.com>
2023-08-08 14:14:05 +0500

    Q-11
    Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
    
    Okay

---
+++ Q11.ts
@@ -0,0 +1,9 @@
+// HEAD
+
+//
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+let names = ['Jawad', 'Mubashir', 'Shahjee'];
+for(let name of names){
+    console.log(name);
+}
+// 8848dc6 (Q-11)
