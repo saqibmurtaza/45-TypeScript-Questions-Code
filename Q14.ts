3ef3176804ed1c004ea528d3b757763010c7f48e
Saqib <saqibmurtazakhan@gmail.com>
2023-08-08 23:23:54 +0500

    Q15 added
    Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite

---
+++ Q14.ts
@@ -0,0 +1,12 @@
+/*Guest List: If you could invite anyone, living or 
+deceased, to dinner, who would you invite? Make a list that 
+includes at least three people you’d like to invite to dinner. 
+Then use your list to print a message to each person, 
+inviting them to dinner. */
+let guestList = ['Jawad', 'Mubashir','Shahjee'];
+for(let name of guestList){
+    console.log(`Dear ${name} join us on dinner`)
+}
+//15: replace a guest
+guestList.splice(2,1 ['Mutahar']);
+
