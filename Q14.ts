aea150a924f97bb5062696c9d0d02bf34d83f294
Saqib <saqibmurtazakhan@gmail.com>
2023-08-09 17:07:41 +0500

    Q15, Q16 & Q17: Codes Added
    Check Readme file for Questions

--- Q14.ts
+++ Q14.ts
@@ -14,3 +14,28 @@ console.log(`\n New Guest List : ${guestList}`)
 guestList.forEach((name)=>{
     console.log(`\n Hello ${name} join us on tonight, dinner time - 8:00 pm`)
 })
+
+// 16: Add 3 more guests; one at start, one at middle & one at end
+
+console.log(`\n Guest List of Q15 : ${guestList}`);
+guestList.unshift('Ahmad');
+console.log(`\n The new list : [${guestList}] after the addition of guest ${guestList[0]}`);
+guestList.splice(2,0,'Waiz');
+console.log(`\n The new list : [${guestList}] after the addition of guest ${guestList[2]}`);
+guestList.push('Izhan');
+console.log(`\n The new list : [${guestList}] after the addition of guest ${guestList[5]}`);
+console.log(`\n Hi! It is to inform all of you that we find a Big Dinnig Tabe \n for dinner, therefore inviting 3 more guests, the guest names are [${guestList[0]},${guestList[2]} & ${guestList[5]}] `)
+guestList.forEach((guest)=>{
+    console.log(`\n Hello ${guest} ! Join us tonight on Dinner`);
+})
+
+//17: Only 2 guests be on Dinner
+
+console.log(`\n With regret, we have to invite only 2 Guests tonight because of Big-Dining-Table cancellation `)
+let guestOutFromList = guestList.splice(2,4);
+guestOutFromList.forEach((guest)=>{
+    console.log(`\n Sorry Mr.${guest}, your invition for tonight dinner is \ncancelled because of Dining-Table-Booking cancellation`);
+})
+for(let guest of guestList){
+    console.log(`\n We are happy to invite you Mr.${guest} on Dinner tonight`);
+}
